0bad9fe335485b5a11d95d0f47f4f3065c082c3c
kazereborn <dark.kazoeru@gmail.com>
2018-01-17 03:49:34 +0800

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,12 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/7.18.0"#"Mozilla/5.0"
-  LA = "IOSIPAD\t7.18.0\tiphone OS\t11.12.1"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
-  #UA = "Line/7.18.0"#"Mozilla/5.0"
-  #LA = "CHROMEOS\t.1.4.5\tChrome_HP\t1"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
-  #UA = "Line/6.0.0 iPad4,1 9.0.2"
-  #LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
+  UA = "Line/1.4.17"
+  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
 
   rev = 0
 
