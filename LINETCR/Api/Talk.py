88cec71813d973ca47328fc343a22dee792fd886
kazereborn <dark.kazoeru@gmail.com>
2018-01-17 02:39:14 +0800

    Add files via upload

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -20,8 +20,12 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Mozilla/5.0"#"Mozilla/5.0"
-  LA = "CHROMEOS\x091.4.13\x09Chrome_OS\x091"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
+  UA = "Line/7.18.0"#"Mozilla/5.0"
+  LA = "IOSIPAD\t7.18.0\tiphone OS\t11.12.1"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
+  #UA = "Line/7.18.0"#"Mozilla/5.0"
+  #LA = "CHROMEOS\t.1.4.5\tChrome_HP\t1"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
+  #UA = "Line/6.0.0 iPad4,1 9.0.2"
+  #LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
 
   authToken = None
   cert = None
--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -18,8 +18,14 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Mozilla/5.0"#"Mozilla/5.0"
-    LA = "CHROMEOS\x091.4.13\x09Chrome_OS\x091"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
+    UA = "Line/7.18.0"#"Mozilla/5.0"
+    #LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
+    #UA = "Line/7.18.0"#"Mozilla/5.0"
+    LA = "IOSIPAD\t7.18.0\tiphone OS\t11.12.1"#"CHROMEOS\x091.4.13\x09Chrome_OS\x091"
+    #UA = "Line/6.0.0 iPad4,1 9.0.2"
+    #LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
+    #UA = "Line/7.18.0 AppleWebKit/534.30"#"Line/6.0.0 iPad4,1 9.0.2"
+    #LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
 
     authToken = None
     mid = None
